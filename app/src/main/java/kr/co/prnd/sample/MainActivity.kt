86eaa0c1f421b1183e8ccb7eb5085aa97957ad1e
olaf-prnd <olaf@prnd.co.kr>
2025-08-29 13:42:06 +0900

    feat: 실습 환경을 구축한다

--- app/src/main/java/kr/co/prnd/sample/MainActivity.kt
+++ app/src/main/java/kr/co/prnd/sample/MainActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import kr.co.prnd.apple.Apple
+import kr.co.prnd.banana.Banana
 import kr.co.prnd.sample.ui.theme.SampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +21,26 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SampleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+    Scaffold { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
+            Apple()
+            Text(text = Banana().getName())
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+private fun Preview() {
     SampleTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
